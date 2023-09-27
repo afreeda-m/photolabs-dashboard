bdf315274d6bf17f81a76da6d7d38c1355c9b33b
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-09-27 12:09:43 -0400

    implement data fetching from the photolabs API using class-based components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 
 import classnames from "classnames";
+import {
+  getTotalPhotos,
+  getTotalTopics,
+  getUserWithLeastUploads,
+  getUserWithMostUploads
+} from "helpers/selectors";
 import Loading from "./Loading";
 import Panel from "./Panel";
 
@@ -9,22 +15,22 @@ const data = [
   {
     id: 1,
     label: "Total Photos",
-    value: 10
+    getValue: getTotalPhotos
   },
   {
     id: 2,
     label: "Total Topics",
-    value: 4
+    getValue: getTotalTopics
   },
   {
     id: 3,
     label: "User with the most uploads",
-    value: "Allison Saeng"
+    getValue: getUserWithMostUploads
   },
   {
     id: 4,
     label: "User with the least uploads",
-    value: "Lukas Souza"
+    getValue: getUserWithLeastUploads
   }
 ];
 
@@ -32,10 +38,27 @@ class Dashboard extends Component {
 
   state = {
     loading: false,
-    focused: null
+    focused: null,
+    photos: [],
+    topics: []
   };
 
   componentDidMount() {
+    const urlsPromise = [
+      "/api/photos",
+      "/api/topics",
+    ].map(url => fetch(url).then(response => response.json()));
+
+
+    Promise.all(urlsPromise)
+    .then(([photos, topics]) => {
+      this.setState({
+        loading: false,
+        photos: photos,
+        topics: topics
+      });
+    });
+
     const focused = JSON.parse(localStorage.getItem("focused"));
 
     if (focused) {
@@ -56,17 +79,21 @@ class Dashboard extends Component {
   }
 
   render() {
-    const dashboardClasses = classnames("dashboard", {"dashboard--focused": this.state.focused});
+    const dashboardClasses = classnames("dashboard", {"dashboard--focused":
+
+    this.state.focused});
 
     if(this.state.loading){
       return <Loading />
     }
 
+    console.log(this.state);
+
     const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data).map(panel => (
       <Panel
         key={panel.id}
         label={panel.label}
-        value={panel.value}
+        value={panel.getValue(this.state)}
         onSelect={event => this.selectPanel(panel.id)}
       />
     ));
